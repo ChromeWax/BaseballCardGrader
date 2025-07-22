4556bb4d1d51239f541b1274ddacce0e39805dd0
Richard Vo <38846128+ChromeWax@users.noreply.github.com>
2025-07-22 03:24:34 -0700

    Add new BaseBallCardGrader.Maui Project

---
+++ src/BaseballCardGrader.Maui/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace BaseballCardGrader.Maui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "BaseballCardGrader.Maui" };
+        }
+    }
+}
