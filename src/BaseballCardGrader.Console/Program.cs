6f110960d284ff12be021e0938e551ec159ba6f4
Richard Vo <38846128+ChromeWax@users.noreply.github.com>
2025-07-31 05:08:32 -0700

    Get Console app working

--- src/BaseballCardGrader.Console/Program.cs
+++ src/BaseballCardGrader.Console/Program.cs
@@ -1,3 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
+using ImageProcessor.DependencyInjection;
+using ImageProcessor.Features.AnalyzeImageForDefects;
+using Mediator;
+using Microsoft.Extensions.DependencyInjection;
 
-Console.WriteLine("Hello, World!");
+namespace BaseballCardGrader.Console;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        if (args.Length != 3)
+        {
+            System.Console.WriteLine("Usage: BaseballCardGrader.Console.exe <input model file> <input image file> <output image path>");
+            return;
+        }
+        
+        var modelFilePath = args[0];
+        var imageFilePath = args[1];
+        var imageFilePath = args[2];
+        
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddImageProcessor();
+
+        var provider = serviceCollection.BuildServiceProvider();
+
+        var sender = provider.GetRequiredService<ISender>();
+
+        var result = await sender.Send(new AnalyzeImageForDefectsRequest(modelFilePath, imageFilePath));
+        result.Save(outputImagePath);
+    }
+}
