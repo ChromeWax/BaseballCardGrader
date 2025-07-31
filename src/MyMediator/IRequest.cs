6b1688c5bd3b02f3770a002575f4737f9f66bd3b
Richard Vo <38846128+ChromeWax@users.noreply.github.com>
2025-07-31 02:08:17 -0700

    Add Mediator library

---
+++ src/MyMediator/IRequest.cs
@@ -0,0 +1,3 @@
+namespace MyMediator;
+
+public interface IRequest<TResponse> {}
---
+++ src/MyMediator/IRequestHandler.cs
@@ -0,0 +1,6 @@
+namespace MyMediator;
+
+public interface IRequestHandler<in TRequest, TResponse> where TRequest : IRequest<TResponse>
+{
+    Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken);
+}
