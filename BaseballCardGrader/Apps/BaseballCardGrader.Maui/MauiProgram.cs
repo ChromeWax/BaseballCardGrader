6e4132565ff0f4bc9f6adc951918d6b3c5349d49
Richard Vo <38846128+ChromeWax@users.noreply.github.com>
2025-08-29 03:06:36 -0700

    Allow images to be saved

--- BaseballCardGrader/Apps/BaseballCardGrader.Maui/MauiProgram.cs
+++ BaseballCardGrader/Apps/BaseballCardGrader.Maui/MauiProgram.cs
@@ -4,6 +4,7 @@ using BaseballCardGrader.Maui.Views;
 using CommunityToolkit.Maui;
 using ImageProcessor.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using CommunityToolkit.Maui;
 
 namespace BaseballCardGrader.Maui
 {
@@ -15,20 +16,16 @@ namespace BaseballCardGrader.Maui
             builder.UseMauiApp<App>().ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-            }).UseMauiCommunityToolkitCamera();
+            }).UseMauiCommunityToolkitCamera().UseMauiCommunityToolkit();
             builder.Services.AddMauiBlazorWebView();
-            
 #if DEBUG
             builder.Services.AddBlazorWebViewDeveloperTools();
             builder.Logging.AddDebug();
 #endif
-
             builder.Services.AddImageProcessor();
             builder.Services.AddSingleton<ApplicationState>();
             builder.Services.AddSingleton<IEsp32BluetoothService, Esp32BluetoothService>();
-
             builder.Services.AddTransient<CaptureImagePage>();
-            
             return builder.Build();
         }
     }
